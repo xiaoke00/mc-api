83a3664783a4361d7166e41a91eca005a770c127
xiaokehuang <xiaokehuang@xiaokes-air.fios-router.home>
2018-03-17 18:14:57 -0400

    rename main class

--- mc-api/src/main/java/com/mastercard/api/Applications.java
+++ mc-api/src/main/java/com/mastercard/api/Applications.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeChallengeApplication {
+public class Applications {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeChallengeApplication.class, args);
+		SpringApplication.run(Applications.class, args);
 	}
 }
--- mc-api/src/test/java/com/mastercard/api/ApplicationsTests.java
+++ mc-api/src/test/java/com/mastercard/api/ApplicationsTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CodeChallengeApplicationTests {
+public class ApplicationsTests {
 
 	@Test
 	public void contextLoads() {
